4a51842850748d64cc9841e1e309c603aeb19e60
Linxusthuis <140453732+Linxusthuis@users.noreply.github.com>
2024-03-06 16:43:44 -0300

    um teste bem teste

---
+++ js.js
@@ -0,0 +1,7 @@
+const ramdom = document.querySelector('--aletorio')
+
+function setProperty(duration){
+    aleatorio.style.setProperty('--aleatorio',duration +'s')
+}
+var ramdom = Math.random(2073600)
+
